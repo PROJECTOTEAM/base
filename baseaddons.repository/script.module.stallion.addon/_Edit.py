12ce843f6629a28d7028a4ed1c6828c2b37b80ef
PROJECTOTEAM <tvsupertuga@gmail.com>
2017-05-21 19:36:58 +0100

    3rtghhgg

--- baseaddons.repository/script.module.stallion.addon/_Edit.py
+++ baseaddons.repository/script.module.stallion.addon/_Edit.py
@@ -1,5 +1,5 @@
 import xbmcaddon
 import base64
 
-MainBase = base64.b64decode ('aHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3LzBDa25wS3VF')
+MainBase = base64.b64decode ('aHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3L0F6dko5UzdG')
 addon = xbmcaddon.Addon('script.module.stallion.addon')
