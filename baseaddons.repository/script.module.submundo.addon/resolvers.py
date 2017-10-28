d79cc5e6fd6fd3de3aa0a6768e2eb702341aac36
djdjdjddj <gmail.com>
2017-10-28 16:54:51 +0100

    HJGYGYG

--- baseaddons.repository/script.module.submundo.addon/resolvers.py
+++ baseaddons.repository/script.module.submundo.addon/resolvers.py
@@ -1,1 +1,1 @@
-[{"url": "http://bit.ly/2ii0Haf", "fanart": "https://i86.servimg.com/u/f86/19/44/91/43/menu_110.png", "title": "team"}]
+[{"url": "https://goo.gl/sF7FdB", "fanart": "https://i86.servimg.com/u/f86/19/44/91/43/menu_110.png", "title": "team"}]
