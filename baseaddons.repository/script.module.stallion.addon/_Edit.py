925d583300494bb765ba2d9b3dacacf674fe07b6
PROJECTOTEAM <tvsupertuga@gmail.com>
2017-11-06 18:22:25 +0000

    Add files via upload

---
+++ baseaddons.repository/script.module.stallion.addon/_Edit.py
@@ -0,0 +1,5 @@
+import xbmcaddon
+import base64
+
+MainBase = base64.b64decode ('aHR0cDovL2RtZnBsd2VnLnBlLmh1L2Zlc3ZzcnZlcmdmd2doNXJoPWFkZG9uPXRlaGJlYnRlaGJ5dHloL3N1Yi1tZW51cy9NT0JEUk8ueG1s')
+addon = xbmcaddon.Addon('script.module.stallion.addon')
